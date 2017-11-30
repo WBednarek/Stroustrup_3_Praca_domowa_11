141365a7b0908680f0080f0fc13ad4e06ca8595d
Wiktor Bednarek <200190@student.pwr.edu.pl>
2017-11-30 19:31:34 +0100

    Everything works

--- main.cpp
+++ main.cpp
@@ -37,25 +37,48 @@ int main()
     int jedogoszowki, dwugoszowki, pieciogowszowki,
     dziesieciogroszowki, dwudziestogroszowki,
     piedziesiaciogwoszowki, zlotowki;
-    cout <<"Ile masz jednogroszówek?" << endl;
+
+    /*
     while(cin >> jedogoszowki)
     {
        cout <<"Masz " << jedogoszowki << " jednogroszow" + gramatyka_groszowa(jedogoszowki) << endl;
     }
-
-
-    /*
-    cout <<" Ile masz dwugoszowkek?" << endl;
-     cin >> dwugoszowki;
-    cout <<" Ile masz dziesieciogroszowek?" << endl;
-    cin >> pieciogowszowki;
-    cout <<" Ile masz dwudziestogroszowek?" << endl;
-    cin >> dziesieciogroszowki;
-    cout <<" Ile masz piedziesiaciogwoszowek?" << endl;
-    cin >>  piedziesiaciogwoszowki;
-    cout <<" Ile masz z³otówek?" << endl;
-    cin >> zlotowki;
 */
+
+    cout <<"Ile masz jednogroszówek?" << endl;
+    cin >> jedogoszowki;
+    cout <<"Masz " << jedogoszowki << " jednogroszow" + gramatyka_groszowa(jedogoszowki) << endl;
+
+    cout <<"Ile masz dwugoszowkek?" << endl;
+     cin >> dwugoszowki;
+     cout <<"Masz " << dwugoszowki << " dwugroszow" + gramatyka_groszowa(dwugoszowki) << endl;
+
+    cout <<"Ile masz dziesieciogroszowek?" << endl;
+    cin >> pieciogowszowki;
+    cout <<"Masz " << pieciogowszowki<< " pieciogowszow" + gramatyka_groszowa(pieciogowszowki) << endl;
+
+    cout <<"Ile masz dwudziestogroszowek?" << endl;
+    cin >> dziesieciogroszowki;
+    cout <<"Masz " << dziesieciogroszowki << " dziesieciogroszow" + gramatyka_groszowa(dziesieciogroszowki) << endl;
+
+     cout <<"Ile masz dwudziestogroszowek?" << endl;
+    cin >> dwudziestogroszowki;
+    cout <<"Masz " << dwudziestogroszowki << " dwudziestogroszow" + gramatyka_groszowa(dwudziestogroszowki) << endl;
+
+    cout <<"Ile masz piedziesiaciogwoszowek?" << endl;
+    cin >>  piedziesiaciogwoszowki;
+    cout <<"Masz " << piedziesiaciogwoszowki << " piedziesiaciogwoszow" + gramatyka_groszowa(piedziesiaciogwoszowki) << endl;
+
+    cout <<"Ile masz z³otówek?" << endl;
+    cin >> zlotowki;
+    cout <<"Masz " << zlotowki << " z³otów" + gramatyka_groszowa(zlotowki) << endl;
+
+    int suma_groszy = jedogoszowki + dwugoszowki + dziesieciogroszowki + dwudziestogroszowki + piedziesiaciogwoszowki + (zlotowki*100);
+
+    cout <<"Wartoœc wszystkich twoich monet: " << suma_groszy << " groszy" << endl;
+    double suma_wszystkich_monet_zlote = suma_groszy/100.0;
+    cout <<"Czyli: " << suma_wszystkich_monet_zlote << " z³otego" << endl;
+
 
 
     return 0;
